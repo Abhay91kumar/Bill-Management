d1e70518ebc551328187ef616b6daffa15ab011e
Abhay Kumar <154663062+Abhay91kumar@users.noreply.github.com>
2025-10-20 11:07:15 +0530

    Change allowed origin for CORS to production URL

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 const Port = process.env.PORT || 5000;
 const app = express();
 
-const allowedOrigin = 'http://localhost:3000';
+const allowedOrigin = 'https://kharti.netlify.app/';
 app.use(cors({
   origin: allowedOrigin,
   credentials: true
