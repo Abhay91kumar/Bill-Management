05ed90919f1ffc69a7e703391141f75535dcee12
Abhay Kumar <154663062+Abhay91kumar@users.noreply.github.com>
2025-10-20 13:47:23 +0530

    Enable secure cookies for refresh tokens

--- server/Controller/userCtrl.js
+++ server/Controller/userCtrl.js
@@ -33,8 +33,8 @@ const userCtrl = {
 
             res.cookie('refreshtoken', refreshToken, {
                 httpOnly: true,
-                // secure: true,
-                // sameSite: 'None',
+                 secure: true,
+                 sameSite: 'None',
                 path: '/user/refresh_token'
             })
 
@@ -77,6 +77,8 @@ const userCtrl = {
 
             res.cookie('refreshtoken', refreshtoken, {
                 httpOnly: true,
+                 secure: true,
+                 sameSite: 'None',
                 path: '/user/refresh_token'
             });
 
