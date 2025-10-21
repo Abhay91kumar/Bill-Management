cd556e86527ffffed27151f2b61dff944833c120
Abhay Kumar <154663062+Abhay91kumar@users.noreply.github.com>
2025-10-21 17:48:20 +0530

    Add authentication middleware to payment routes

--- server/Routers/paymentRouter.js
+++ server/Routers/paymentRouter.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const paymentCtrl = require('../Controller/paymentCtrl');
 
 
-router.post('/create', paymentCtrl.createPayment);
-router.get('/', paymentCtrl.getPayments);
-router.put('/:id', paymentCtrl.updatePayment);
-router.delete('/:id', paymentCtrl.deletePayment);
+router.post('/create', auth, paymentCtrl.createPayment);
+router.get('/', auth, paymentCtrl.getPayments);
+router.put('/:id', auth, paymentCtrl.updatePayment);
+router.delete('/:id', auth, paymentCtrl.deletePayment);
 
 module.exports = router;
